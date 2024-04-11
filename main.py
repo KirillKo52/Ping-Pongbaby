a4dc49ba0dcc73f3792ee5c8491d027005bde9e1
KirillKo52 <166024857+KirillKo52@users.noreply.github.com>
2024-04-11 19:23:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ font1 = font.SysFont('Arial', 36)
 lost = 0
 win = 0
 HP = 3
-font2 = font.SysFont('Arial', 100)
-window = display.set_mode((700,700))
+font2 = font.SysFont('Arial', 50)
+window = display.set_mode((500,500))
 display.set_caption('Maze')
-background = transform.scale(image.load("ZadniyFon.jpg"),(700,500))
-mixer.init()
+background = transform.scale(image.load("images.jpeg"),(500,500))
+'''mixer.init()
 mixer.music.load('game.ogg')
-mixer.music.play()
+mixer.music.play()'''
 run = True
 clock = time.Clock()
 FPS = 60
 finish = True
-fake = True
 class GameSprite(sprite.Sprite):
-    def __init__(self,player_image,player_x,player_y,player_speed,player_weight,player_height):
+    def __init__(self,player_image,player_x,player_y,player_speedx,player_speedy,player_weight,player_height):
         super().__init__()
         self.image = transform.scale(image.load(player_image),(player_weight, player_height))
-        self.speed = player_speed
+        self.speed.x = player_speedx
+        self.speed.y = player_speedy
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -34,23 +34,43 @@ class GameSprite(sprite.Sprite):
 class Player1(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_DOWN] and self.rect.y < 615:
-            self.rect.x += self.speed
+        if keys_pressed[K_w] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
 class Player2(GameSprite):
     def update(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_w] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys_pressed[K_s] and self.rect.y < 615:
-            self.rect.x += self.speed        
-class Enemy(GameSprite):
+        if keys_pressed[K_UP] and self.rect.y > 0:
+            self.rect.y -= self.speed
+        if keys_pressed[K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed        
+class Ball(GameSprite):
     def update(self):
-        self.rect.y += self.speed
-        global lost
-        if self.rect.y > 500:
-            self.rect.y = 0
-            self.rect.x = randint(0,620)
-            lost = lost + 1
-        self.speed = randint(1,2)
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.y = 450:
+            self.speedy * -1
+        if self.rect.y = 50:
+            self.speedy * -1
+        if self.rect.x = 70:
+            self.speedx * -1
+        if self.rect.x = 420:
+            self.speedx * -1
+player1 = Player1('player2.jpg',70,250,5,20,150)
+player2 = Player2('player2.jpg',420,250,5,20,150)
+ball = Ball('Ball.jpg',250,350,1,70,70)
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run= False
+    if run != False:
+        window.blit(background,(0,0))
+        player1.update()
+        player1.reset()
+        player2.update()
+        player2.reset()
+        ball.update()
+        ball.reset()
+    clock.tick(FPS)
+    display.update()
